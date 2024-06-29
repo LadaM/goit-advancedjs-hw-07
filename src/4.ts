82045e008b16ab16de05866122b86478dcd6de7c
Lada <lada11lada@gmail.com>
2024-06-29 21:35:37 +0200

    HW 7 - tasks 1-4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,47 @@
+class Key {
+    private readonly signature: number;
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+abstract class House {
+    private door: boolean;
+    constructor(private readonly key: Key, public readonly tenants: Person[] = []) {
+        this.key = key;
+        this.door = false;
+    }
+
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+}
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+}
+class Person {
+    constructor(private readonly key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
